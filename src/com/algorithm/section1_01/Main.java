080c4503eb3fc1472d429703543db28dcc62ad3b
conagreen <icoqwee@gmail.com>
2021-06-24 09:15:25 +0900

    20210624 - 문자 찾기

--- src/com/algorithm/section1_01/Main.java
+++ src/com/algorithm/section1_01/Main.java
@@ -1,8 +1,32 @@
 package com.algorithm.section1_01;
 
+import java.util.Scanner;
+
 public class Main {
+    public int solution(String str, char chr) {
+        int count = 0;
+
+        str = str.toUpperCase();
+        chr = Character.toUpperCase(chr);
+
+        for (char c : str.toCharArray()) {
+            if (c == chr)
+                count++;
+        }
+
+        return count;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        Main m = new Main();
+
+        Scanner scanner = new Scanner(System.in);
+
+        String str = scanner.next();
+        char chr = scanner.next().charAt(0);
+
+        int result = m.solution(str, chr);
+
+        System.out.print(result);
     }
 }
