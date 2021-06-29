3f21a8c28a60b3a85cd8de7b7cc198450017c50a
conagreen <icoqwee@gmail.com>
2021-06-30 08:20:24 +0900

    20210630 - 회문 문자열

--- README.md
+++ README.md
@@ -19,7 +19,7 @@
 4. [단어 뒤집기 (StringBuilder 이용법 또는 직접뒤집기)](src/com/algorithm/section1/section1_04)
 5. [특정 문자 뒤집기 (toCharArray())](src/com/algorithm/section1/section1_05)
 6. [중복문자제거](src/com/algorithm/section1/section1_06)
-7. 회문문자열
+7. [회문문자열](src/com/algorithm/section1/section1_07)
 8. 유효한 팰린드롬 (replaceAll 정규식이용)
 9. 숫자만 추출
 10. 가장 짧은 문자거리
---
+++ src/com/algorithm/section1/section1_07/Main.java
@@ -0,0 +1,48 @@
+package com.algorithm.section1.section1_07;
+
+import java.util.Scanner;
+
+public class Main {
+
+    // StringBuilder 없이 구현
+//    public String solution1(String str) {
+//        str = str.toUpperCase();
+//        String result = "YES";
+//        int len = str.length();
+//
+//        for (int i = 0, limit = len/2; i < limit; i++) {
+//            if (str.charAt(i) != str.charAt(len-i-1)) {
+//                result = "NO";
+//                break;
+//            }
+//        }
+//
+//        return result;
+//    }
+
+    // StringBuilder로 구현
+    private String solution2(String str) {
+        String result = "NO";
+        String temp = new StringBuilder(str).reverse().toString();
+
+        if (str.equalsIgnoreCase(temp)){
+            result = "YES";
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Main m = new Main();
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+
+        // StringBuilder 없이 구현
+//        System.out.println(m.solution1(str));
+
+        // StringBuilder로 구현
+        System.out.println(m.solution2(str));
+
+
+    }
+}
