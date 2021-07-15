148bc20382cfae587815193bbdea8523b185c2b6
conagreen <icoqwee@gmail.com>
2021-07-15 18:13:10 +0900

    20210715 - 공통원소 구하기

--- README.md
+++ README.md
@@ -49,7 +49,7 @@
 <summary>Section 3. Tow pointers, Sliding window</summary>
 
 1. [두 배열 합치기 (two pointers algorithm)](src/com/algorithm/section3/section3_01)
-2. 공통원소구하기 (two pointers algorithm)
+2. [공통원소구하기(two pointers algorithm)](src/com/algorithm/section3/section3_02)
 3. 최대 매출 (Sliding window)
 4. 연속부분수열 (복합적 문제)
 5. 연속된 자연수의 합 (two pointers)
---
+++ src/com/algorithm/section3/section3_02/Main.java
@@ -0,0 +1,60 @@
+package com.algorithm.section3.section3_02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        Main m = new Main();
+
+        int num1 = Integer.parseInt(br.readLine());
+        int[] numArray1 = m.readNumbers(num1);
+
+        int num2 = Integer.parseInt(br.readLine());
+        int[] numArray2 = m.readNumbers(num2);
+
+        for (int result : m.solution(num1, numArray1, num2, numArray2)) {
+            System.out.print(result + " ");
+        }
+
+    }
+
+    private int[] readNumbers(int num) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] numArray = new int[num];
+
+        for (int i = 0; i < num; i++) {
+            numArray[i] = Integer.parseInt(st.nextToken());
+        }
+
+        return numArray;
+    }
+
+    private List<Integer> solution(int num1, int[] numArray1, int num2, int[] numArray2) {
+        List<Integer> result = new ArrayList<>();
+        Arrays.sort(numArray1);
+        Arrays.sort(numArray2);
+        int p1 = 0, p2 = 0;
+
+        while (p1 < num1 && p2 < num2) {
+            if (numArray1[p1] == numArray2[p2]) {
+                result.add(numArray1[p1++]);
+                p2++;
+            } else if (numArray1[p1] < numArray2[p2]){
+                p1++;
+            } else {
+                p2++;
+            }
+        }
+
+        return result;
+    }
+}
