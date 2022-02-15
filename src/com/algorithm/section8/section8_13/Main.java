beab10de973d0dc26f6ad54f6d24ea21c7ae885f
conagreen <icoqwee@gmail.com>
2022-02-15 18:30:37 +0900

    20220215 - 섬나라 아일랜드(DFS)

---
+++ src/com/algorithm/section8/section8_13/Main.java
@@ -0,0 +1,50 @@
+package com.algorithm.section8.section8_13;
+
+import java.util.Scanner;
+
+public class Main {
+    static int n, result;
+    static int[][] board;
+    int[] lp = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int[] cp = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scanner = new Scanner(System.in);
+
+        n = scanner.nextInt();
+        board = new int[n+2][n+2];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                board[i][j] = scanner.nextInt();
+            }
+        }
+
+        main.solution();
+        System.out.println(result);
+    }
+
+    private void solution() {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (board[i][j] == 1) {
+                    result++;
+                    board[i][j] = 0;
+                    DFS(i, j);
+                }
+            }
+        }
+    }
+
+    private void DFS(int low, int col) {
+        for (int i = 0; i < 8; i++) {
+            int curLow = low + lp[i];
+            int curCol = col + cp[i];
+            if (board[curLow][curCol] == 1) {
+                board[curLow][curCol] = 0;
+                DFS(curLow, curCol);
+            }
+        }
+    }
+}
