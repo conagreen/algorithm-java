3130affa5ab2583d704ef20cf9c3af134592275c
conagreen <icoqwee@gmail.com>
2022-05-04 01:06:29 +0900

    20220503 - 그룹 단어 체커

---
+++ src/com/algorithm/level_01/그룹_단어_체커/Main.java
@@ -0,0 +1,40 @@
+package com.algorithm.level_01.그룹_단어_체커;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
+        }
+
+        final int result = main.solution(arr);
+        System.out.println(result);
+    }
+
+    private int solution(String[] arr) {
+        int result = 0;
+        for (String word : arr) {
+            if (isGroupWord(word)) result++;
+        }
+        return result;
+    }
+
+    private boolean isGroupWord(String word) {
+        char[] alpha = word.toCharArray();
+
+        for (int i = 0; i < word.length()-1; i++) {
+            if (alpha[i] != alpha[i+1]) {
+                if (word.indexOf(alpha[i], i+1) > i) return false;
+            }
+        }
+        return true;
+    }
+}
