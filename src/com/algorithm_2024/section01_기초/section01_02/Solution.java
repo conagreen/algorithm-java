7a8431edef67297a0c7274148774fed48b07115a
conagreen <verycona@gmail.com>
2024-05-28 23:37:13 +0900

    O(N^2) -> O(N) 으로 시간복잡도 개선

--- src/com/algorithm_2024/section01_기초/section01_02/Solution.java
+++ src/com/algorithm_2024/section01_기초/section01_02/Solution.java
@@ -1,20 +1,34 @@
 package com.algorithm_2024.section01_기초.section01_02;
 
 /*
-* 주어진 길이 n의 int 배열 arr에서 합이 100인 서로 다른 위치의 두 원소가 존재하면 1을, 그렇지 않으면 0을 반환 -> O(N^2)
+* 주어진 길이 n의 int 배열 arr에서 합이 100인 서로 다른 위치의 두 원소가 존재하면 1을, 그렇지 않으면 0을 반환
 * */
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(func(new int[]{1, 52, 48}, 3));
+        System.out.println(fun1(new int[]{1, 52, 48}, 3));
+        System.out.println(func2(new int[]{1, 52, 48}, 3));
     }
 
-    private static long func(int[] arr, int n) {
+    // O(N^2)
+    private static long fun1(int[] arr, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] + arr[j] == 100) {
                     return 1;
                 }
             }
+        }
+        return 0;
+    }
+
+    // O(N)
+    private static long func2(int[] arr, int n) {
+        int[] template = new int[101];
+        for (int i = 0; i < n; i++) {
+            if (template[100 - arr[i]] == 1) {
+                return 1;
+            }
+            template[arr[i]] = 1;
         }
         return 0;
     }
